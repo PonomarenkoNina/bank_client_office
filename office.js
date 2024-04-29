1c912b41648bcb3d7196c97aa53212f3f23402d4
PonomarenkoNina <ninulka@ukr.net>
2024-04-29 16:12:51 +0300

    View changed for folder

--- office.js
+++ office.js
@@ -2,9 +2,9 @@ var clients = [
   {
     name: "Volodymyr Parkhomenko",
     surname: "Olegovich",
-    login: +380671234567,
-    password: true,
-    cardPrivatbank: {
+    login: "+380671234567",
+    password: "Volod123",
+    cardKredobank: {
       number: 4149243556435478,
       balance: "46000 UAH",
     },
@@ -12,26 +12,57 @@ var clients = [
   {
     name: "Olga Musaeva",
     surname: "Volodymyrivna",
-    login: +380677654321,
-    password: false,
-    cardPrivatbank: {
+    login: "+380677654321",
+    password: "Olga1234",
+    cardKredobank: {
       number: 4149356468907654,
       balance: "100000 UAH",
     },
   },
 ];
 //Викликаємо функцію бази даних клієнтів
-function customerDatabase(client) {
-  var currentClient;
-  if (client.login === true) {
-    currentClient = function () {
-      alert("");
-    };
+function customerDatabase() {
+  var loginInput = document.getElementById("loginInput").value;
+  var passwordInput = document.getElementById("passwordInput").value;
+  var isValidClient = false;
+
+  for (var i = 0; i < clients.length; i++) {
+    if (
+      clients[i].login === loginInput &&
+      clients[i].password === passwordInput
+    ) {
+      isValidClient = true;
+      window.location.href = "office_2.html"; // Перенаправлення на сторінку 2
+      break;
+    }
+  }
+  //Перевірка логіну та паролю
+  if (!isValidClient) {
+    alert("Ви ввели неправильний пароль або логін!");
   }
 }
+function showPersonalPage(client) {
+  // Відображення інформації про клієнта на сторінці особистого кабінету
+  document.getElementById("header").innerHTML =
+    "<h1 class='title'>Ласкаво просимо, " +
+    client.surname +
+    " " +
+    client.name +
+    " " +
+    client.patronymic +
+    "</h1>" +
+    "<button class='btn btn-danger' id='logoutButton'>Вийти</button>";
 
-//<h2 class="fw-light">Введіть логін та пароль:</h2>
-//<input type="login" id="loginInput" />
-//<input type="password" id="passwordInput" />
-//<button id="myButton" onclick="customerDatabase()">Логін</button>
-//<p id="output"></p>
+  document.getElementById("cardNumber").innerText =
+    "Номер картки: " + client.cardKredobank.number;
+  document.getElementById("cardBalance").innerText =
+    "Баланс на картці: " + client.cardKredobank.balance;
+
+  // Додавання обробника подій для кнопки "Вийти"
+  document
+    .getElementById("logoutButton")
+    .addEventListener("click", function () {
+      window.location.href = "office_1.html"; // Перенаправлення на сторінку 1
+    });
+}
+document.getElementById("myButton").addEventListener("click", customerDatabase);
